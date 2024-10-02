4a2aacb127eb87f8da6885c0080af16768eff1a4
Ricardo Bañuelos De La Torre <47264662+RicardoBanuelos@users.noreply.github.com>
2024-10-01 18:49:49 -0700

    added DEC

---
+++ src/Instruction/Instructions/DEC.cpp
@@ -0,0 +1,23 @@
+#include "Instruction/Instructions/DEC.h"
+
+DEC::DEC(std::shared_ptr<ICPU> icpu, AddressingMode addressingMode, uint8_t cycles)
+    :   Instruction("DEC", icpu, addressingMode, cycles)
+{
+}
+
+DEC::~DEC()
+{
+}
+
+void DEC::run()
+{
+    AddressingData addressingData = mIcpu->addressing(mAddressingMode);
+    uint8_t fetched = addressingData.data;
+
+    --fetched;
+
+    mIcpu->writeByte(addressingData.address, fetched);
+
+    mIcpu->setFlag(Flag::Z, fetched == 0);
+    mIcpu->setFlag(Flag::N, fetched & 0x80);
+}
