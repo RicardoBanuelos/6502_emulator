396fa5037e0c54efa7896c694b7930db38da071b
Ricardo Bañuelos De La Torre <47264662+RicardoBanuelos@users.noreply.github.com>
2024-10-01 20:09:02 -0700

    added JMP

---
+++ src/Instruction/Instructions/JMP.cpp
@@ -0,0 +1,17 @@
+#include "Instruction/Instructions/JMP.h"
+#include "JMP.h"
+
+JMP::JMP(std::shared_ptr<ICPU> icpu, AddressingMode addressingMode, uint8_t cycles)
+    :   Instruction("JMP", icpu, addressingMode, cycles)
+{
+}
+
+JMP::~JMP()
+{
+}
+
+void JMP::run()
+{
+    AddressingData addressingData = mIcpu->addressing(mAddressingMode);
+    mIcpu->setRegister(Register::PC, addressingData.data);
+}
