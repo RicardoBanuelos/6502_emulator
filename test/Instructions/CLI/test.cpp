015af3c070f5d327db8cec33dd5ee0774f89c521
Ricardo Bañuelos De La Torre <47264662+RicardoBanuelos@users.noreply.github.com>
2024-10-03 11:01:45 -0700

    added CLI unit test

---
+++ test/Instructions/CLI/test.cpp
@@ -0,0 +1,35 @@
+#include <gtest/gtest.h>
+#include "Instruction/Instructions/CLI.h"
+#include "CPU/CPU.h"
+#include "Memory/Memory.h"
+#include "Bus/Bus.h"
+
+static std::shared_ptr<ICPU> cpu(new CPU());
+static std::shared_ptr<Memory> mem(new Memory());
+static std::shared_ptr<Bus> bus(new Bus());
+
+TEST(instructions, cli_implied)
+{
+    cpu->reset();
+    cpu->setFlag(Flag::I, 1);
+
+    std::unique_ptr<CLI> instruction(new CLI(cpu, AddressingMode::Implied, 2));
+    instruction->run();
+
+    ASSERT_EQ(cpu->getFlag(Flag::I), 0);
+}
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    mem->initialize();
+    mem->randomize();
+
+    CPU *cppu = static_cast<CPU*>(cpu.get());
+    cppu->init();
+
+    cppu->connectBus(bus);
+    bus->connectMemory(mem);
+   
+    return RUN_ALL_TESTS();
+}
